1045142e22ccceea9e6d8473bd93d9fb8b7d5466
Cameron O'Brien <obriecam@oregonstate.edu>
2019-12-11 13:51:43 -0800

    trying to merge

--- public/index.js
+++ public/index.js
@@ -1,72 +1,4 @@
 
-var allElems = [];
-
-
-function parseBoardElem(elem){
-  var elem = {
-    name: elem.getAttribute('data-name'),
-    score: elem.getAttribute('data-score'),
-    photoURL: elem.getAttribute('data-photoURL'),
-    place: elem.getAttribute('data-place')
-  }
-  return(elem);
-}
-
-function insertLoser(score, name, counter){
-  var newElem = Handlebars.templates.loserCard({
-    place: counter,
-    score: score,
-    name: name
-  });
-  var container = document.getElementById('losers');
-  container.insertAdjacentHTML('beforeend', newElem);
-}
-
-function insertNewElem(photoURL, score, name){
-  var newElem = Handlebars.templates.scoreCard({
-    photoURL: photoURL,
-    name: name,
-    score: score
-  });
-  var container = document.getElementById('cards');
-  container.insertAdjacentHTML('beforeend', newElem);
-}
-
-var boardElems = document.getElementsByClassName('card');
-for(var i = 0; i<boardElems.length; i++){
-  allElems.push(parseBoardElem(boardElems[i]));
-}
-
-var swapped, temp;
-
-for(var i = 0; i < allElems.length - 1; i++){
-  swapped = 0;
-  for(var j = 0; j < allElems.length - i - 1; j++){
-    if(allElems[j].score < allElems[j + 1].score){
-      temp = allElems[j];
-      allElems[j] = allElems[j + 1];
-      allElems[j + 1] = temp;
-      swapped = 1;
-      console.log("SWAPPED");
-    }
-  }
-  if(swapped == 1){break;}
-}
-
-var postContainer = document.getElementById('cards');
-  while(postContainer.lastChild) {
-    postContainer.removeChild(postContainer.lastChild);
-  }
-var counter = 1;
-allElems.forEach(function (scoreCard){
-  if(counter <= 3){
-    insertNewElem(scoreCard.photoURL, scoreCard.score, scoreCard.name);
-  }
-  else{
-    insertLoser(scoreCard.score, scoreCard.name, counter);
-  }
-  counter += 1;
-});
 
 function organizeData(scoreData){
   var newData;
@@ -86,14 +18,14 @@ function organizeData(scoreData){
 //console.log("Script link success!");
 
 // Object creation helper function from JavaScript: The Good Parts
-/*
+
 if (typeof Object.create !== 'function') {
     Object.create = function (o) {
         var F = function () { };
         F.prototype = o;
         return new F();
     }
-};*/
+};
 
 // an object to hold all of the variables for the blackjack app
 // to avoid global variable drama
