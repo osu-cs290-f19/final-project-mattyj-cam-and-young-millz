0bee14e1d92c2303dfc22e5c56d6a3760234b8e3
Cameron O'Brien <obriecam@oregonstate.edu>
2019-12-11 13:31:39 -0800

    ou

--- public/index.js
+++ public/index.js
@@ -1,3 +1,73 @@
+
+var allElems = [];
+
+
+function parseBoardElem(elem){
+  var elem = {
+    name: elem.getAttribute('data-name'),
+    score: elem.getAttribute('data-score'),
+    photoURL: elem.getAttribute('data-photoURL'),
+    place: elem.getAttribute('data-place')
+  }
+  return(elem);
+}
+
+function insertLoser(score, name, counter){
+  var newElem = Handlebars.templates.loserCard({
+    place: counter,
+    score: score,
+    name: name
+  });
+  var container = document.getElementById('losers');
+  container.insertAdjacentHTML('beforeend', newElem);
+}
+
+function insertNewElem(photoURL, score, name){
+  var newElem = Handlebars.templates.scoreCard({
+    photoURL: photoURL,
+    name: name,
+    score: score
+  });
+  var container = document.getElementById('cards');
+  container.insertAdjacentHTML('beforeend', newElem);
+}
+
+var boardElems = document.getElementsByClassName('card');
+for(var i = 0; i<boardElems.length; i++){
+  allElems.push(parseBoardElem(boardElems[i]));
+}
+
+var swapped, temp;
+
+for(var i = 0; i < allElems.length - 1; i++){
+  swapped = 0;
+  for(var j = 0; j < allElems.length - i - 1; j++){
+    if(allElems[j].score < allElems[j + 1].score){
+      temp = allElems[j];
+      allElems[j] = allElems[j + 1];
+      allElems[j + 1] = temp;
+      swapped = 1;
+      console.log("SWAPPED");
+    }
+  }
+  if(swapped == 1){break;}
+}
+
+var postContainer = document.getElementById('cards');
+  while(postContainer.lastChild) {
+    postContainer.removeChild(postContainer.lastChild);
+  }
+var counter = 1;
+allElems.forEach(function (scoreCard){
+  if(counter <= 3){
+    insertNewElem(scoreCard.photoURL, scoreCard.score, scoreCard.name);
+  }
+  else{
+    insertLoser(scoreCard.score, scoreCard.name, counter);
+  }
+  counter += 1;
+});
+
 function organizeData(scoreData){
   var newData;
   for(var i = 0; i < scoreData.length; i++ ){
@@ -118,7 +188,7 @@ var newGame = function () {
         jsbApp.gameStatus = 1; // to cause the dealer's hand to be drawn face up
         drawHands();
         jsbApp.textUpdates.innerHTML = "You won! You got 21 on your initial hand!";
-        player.balance -= player.bet;
+        player.balance += player.bet;
         track();
         jsbApp.gameStatus = 2; // game is won
         return;
@@ -520,7 +590,7 @@ var cashOutButton = document.getElementById('cash-out-button');
 if(cashOutButton){
   cashOutButton.addEventListener('click', function(){
     var modalHeader3 = document.getElementById('player-score-header');
-    player.modalHeader3.innerHTML = "<h3>Player Score: " + player.balance + "</h3>";
+    //player.modalHeader3.innerHTML = "<h3>Player Score: " + player.balance + "</h3>";
     showCashOutModal.classList.remove('hidden');
     showCashOutModalBackground.classList.remove('hidden');
 
